6fc7b77aa71e5a287a251ea5083031a91ddba8c7
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-02-06 13:08:54 -0600

    Some Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Project Start
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+      </Routes>
     </div>
   );
 }
