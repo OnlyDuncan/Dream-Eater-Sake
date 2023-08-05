8b4afc8104c1e86c988aa3da6b045b2102d99fbe
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-08-05 14:48:25 -0500

    Cleaned Up Some Code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,10 +11,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
   </React.StrictMode>
 );
